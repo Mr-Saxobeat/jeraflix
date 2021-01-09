5ad95c086d37298bab6d8c747551a59d5b41b85d
Weiglas <weiglasribeiro@gmail.com>
2021-01-09 12:42:39 -0300

    Registra model 'Profile' em admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from accounts.models import CustomAccount
+from accounts.models import CustomAccount, Profile
 
 class CustomAccountAdmin(UserAdmin):
     model = CustomAccount
     list_display = ['username', 'email', 'is_staff']
 
 admin.site.register(CustomAccount, CustomAccountAdmin)
+admin.site.register(Profile)
