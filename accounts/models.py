a49e5a4286011850161183b31aeefdb253e8575f
Weiglas <weiglasribeiro@gmail.com>
2021-01-09 12:42:24 -0300

    Cria model 'Profile'

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,12 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class CustomAccount(AbstractUser):
     pass
+
+class Profile(models.Model):
+    account = models.ForeignKey(CustomAccount, on_delete=models.CASCADE, related_name='profiles')
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{ self.account.username } - { self.name }'
