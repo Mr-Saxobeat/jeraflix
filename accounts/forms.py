f01eaa49b192ef0c0540894d6a03729a397c42cf
Weiglas <weiglasribeiro@gmail.com>
2021-01-09 12:43:22 -0300

    Adiciona field 'birthday' ao AccountForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,10 @@
+from django import forms
 from django_registration.forms import RegistrationForm
 from accounts.models import CustomAccount
 
 class AccountForm(RegistrationForm):
+    birthday = forms.DateField(label='Data de nascimento')
 
     class Meta(RegistrationForm.Meta):
         model = CustomAccount
+        fields = ('email', 'password1', 'password2', 'username', 'birthday')
