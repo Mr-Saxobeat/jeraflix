56832b2e9195573f34373b949330dd62b8f0f048
Weiglas <weiglasribeiro@gmail.com>
2021-01-11 19:40:01 -0300

    Move 'CustomLoginView' para 'accounts/views.py'

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth import login, logout, authenticate
+from accounts.forms import LoginAccountForm
+from django.shortcuts import render, redirect
+
+def CustomLoginView(request):
+    if request.method == 'GET':
+        form = LoginAccountForm()
+        return render(request, 'registration/login.html', { 'form': form})
+    elif request.method == 'POST':
+        user = authenticate(request, email=request.POST['email'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'registration/login.html', { 'form': LoginAccountForm(), 'error': 'Email ou senha incorretos.'})
+        else:
+            login(request, user)
+            return redirect('/')
+
+    return render(request, 'registration/login.html', { 'form': form })
+
