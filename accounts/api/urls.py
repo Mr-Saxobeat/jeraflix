d22f4df79b12fd6f94b7a8ce6ed7dd06c9827e4e
Weiglas <weiglasribeiro@gmail.com>
2021-01-13 11:37:11 -0300

    Adiciona url para profile

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from accounts.api.views import CurrentAccountAPIView
+from accounts.api import views
 
 urlpatterns = [
     # Return the current account information
-    path('user/', CurrentAccountAPIView.as_view(), name='current-account'),
+    path('user/', views.CurrentAccountAPIView.as_view(), name='current-account'),
+
+    path('profiles/', views.ListCreateProfileView.as_view(), name='profile-list'),
+
+    path('profile/<int:pk>/', views.RestrieveProfileView.as_view(), name='profile-detail'),
 ]
