44f724d75259234517b0fc46b7c935cf8ed8fd5a
Weiglas <weiglasribeiro@gmail.com>
2021-01-13 11:38:24 -0300

    Adciona paginação

--- conf/settings.py
+++ conf/settings.py
@@ -167,9 +167,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
 }
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
-    'accounts.auth.EmailBackend'
+    'accounts.auth.EmailBackend',
     ]
