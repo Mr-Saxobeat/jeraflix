876336adcf0eecd14cc062fdcec805945288aac3
Weiglas <weiglasribeiro@gmail.com>
2021-01-13 11:35:32 -0300

    Cria 'ProfileSerializer' e adiciona o devido campo para AccountSerializer

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from accounts.models import CustomAccount
+from accounts.models import CustomAccount, Profile
+
+class ProfileSerializer(serializers.ModelSerializer):
+    main_profile = serializers.BooleanField(read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = '__all__'
 
 class CustomAccountDisplaySerializer(serializers.ModelSerializer):
+    profiles = ProfileSerializer(many=True, read_only=True)
     
     class Meta:
         model = CustomAccount
-        fields = ['username']
+        fields = ['id', 'username', 'profiles']
