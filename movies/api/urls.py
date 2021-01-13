d4f251147c78d9fd4f7a1331bab416edd9cff2a5
Weiglas <weiglasribeiro@gmail.com>
2021-01-13 11:37:46 -0300

    Adiciona urls para movies

---
+++ movies/api/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from movies.api import views
+
+urlpatterns = [
+    path('popular/', 
+        views.ListPopularMoviesAPIView.as_view(),
+        name='popular-movies'),
+
+    path('search/',
+        views.SearchMoviesAPIView.as_view(),
+        name='search-movies'),
+
+    path('watchlist/',
+        views.UserWatchListAPIView.as_view(),
+        name='watchlist'),
+]
+
+
