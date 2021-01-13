a54dceacd8bcc6b10b8bb5b145c75bc828b9fee5
Weiglas <weiglasribeiro@gmail.com>
2021-01-13 11:37:34 -0300

    Remove url desnecessária

--- movies/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from movies.api import views
-
-urlpatterns = [
-    path('popular/', 
-        views.PopularMoviesAPIView.as_view(),
-        name='popular-movies'),
-]
