544e8fbcee2b68544291ef0b0111c7fef19cb6ae
Weiglas <weiglasribeiro@gmail.com>
2021-01-14 11:14:58 -0300

    Adiciona secretkey pro heroky

--- conf/settings.py
+++ conf/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '&-suvj4pk-b)%x3t7m%hbll46_15j!r$_#oye0zcw!e^8xq%fe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
